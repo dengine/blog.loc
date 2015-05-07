e35e581a92a9a53493c7979f6bb6df052b6aaaff
dengine <denginestudios@gmail.com>
2015-05-08 01:56:27 +0300

    Rejected commit: Default

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+
+            <h1>Добро пожаловать на блог веб программиста</h1>
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Блог </title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+@yield('content')
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</body>
+</html>
